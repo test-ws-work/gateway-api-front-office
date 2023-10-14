1aac7356d3d4e8836f8d896c590d9518049e35a6
gusferreira <gustavo.ferreira@nutrip.com.br>
2023-10-14 13:52:15 -0300

    feat: create integration with ms-customers and all methods

---
+++ src/customers/controllers/customers.controller.ts
@@ -0,0 +1,74 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CustomersService } from '../services/customers.service';
+import { CustomerDtoRequest } from '../dto/requests/customer-request.dto';
+import { UpdateCustomerDto } from '../dto/requests/update-customer.dto';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CustomerDtoResponse } from '../dto/responses/customer-response.dto';
+import { CustomerLoginDtoRequest } from '../dto/requests/customer-login-request.dto';
+
+@ApiTags('Customer')
+@Controller('api/v1/customers')
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Post(':storeId')
+  @ApiResponse({
+    status: 201,
+    description: 'Create a new sale',
+    type: CustomerDtoResponse,
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request' })
+  create(
+    @Param('storeId') storeId: number,
+    @Body() createCustomerDto: CustomerDtoRequest,
+  ) {
+    return this.customersService.create(storeId, createCustomerDto);
+  }
+
+  @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Return user to login',
+    type: CustomerDtoResponse,
+  })
+  @ApiResponse({ status: 400, description: 'Invalid request' })
+  findByEmailAndPassword(
+    @Query('storeId') storeId: number,
+    @Body() dto: CustomerLoginDtoRequest,
+  ) {
+    return this.customersService.findByEmailAndPassword(storeId, dto);
+  }
+
+  @Get(':customerId')
+  @ApiResponse({
+    status: 200,
+    description: 'Search customer by id',
+    type: CustomerDtoResponse,
+  })
+  @ApiResponse({ status: 404, description: 'Customer not found.' })
+  findCustomerById(@Param('customerId') customerId: number) {
+    return this.customersService.findCustomerById(customerId);
+  }
+
+  @Patch(':customerId')
+  update(
+    @Param('customerId') customerId: number,
+    @Body() updateCustomerDto: UpdateCustomerDto,
+  ) {
+    return this.customersService.update(customerId, updateCustomerDto);
+  }
+
+  @Delete(':customerId')
+  remove(@Param('customerId') customerId: number) {
+    return this.customersService.remove(customerId);
+  }
+}
---
+++ src/customers/services/customers.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CustomerDtoRequest } from '../dto/requests/customer-request.dto';
+import { UpdateCustomerDto } from '../dto/requests/update-customer.dto';
+import { CustomerClient } from '../clients/customer.client';
+import { CustomerLoginDtoRequest } from '../dto/requests/customer-login-request.dto';
+
+@Injectable()
+export class CustomersService {
+  constructor(private readonly customerClient: CustomerClient) {}
+
+  async create(storeId: number, createCustomerDto: CustomerDtoRequest) {
+    return this.customerClient.create(storeId, createCustomerDto);
+  }
+
+  async findByEmailAndPassword(storeId: number, dto: CustomerLoginDtoRequest) {
+    return this.customerClient.findByEmailAndPassword(storeId, dto);
+  }
+
+  async findCustomerById(customerId: number) {
+    return this.customerClient.findCustomerById(customerId);
+  }
+
+  async update(customerId: number, updateCustomerDto: UpdateCustomerDto) {
+    return this.customerClient.update(customerId, updateCustomerDto);
+  }
+
+  async remove(customerId: number) {
+    return this.customerClient.delete(customerId);
+  }
+}
